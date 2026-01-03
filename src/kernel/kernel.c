815ab90168edb43246329e38c0b09130d8fc0558
ooe1220 <earthen94@gmail.com>
2026-01-03 15:52:55 +0800

    各初期化成功後完了文言を画面表示する

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -28,15 +28,21 @@ void kernel_main() {
         
     idt_init(); // IDT初期化
     kputs("IDT initialized\n");
+    
     pic_init(); // PIC初期化
     kputs("PIC initialized\n");
     
     pic_unmask_irq(1); // PICのIRQ1(キーボード割り込み)を有効化
+    kputs("IRQ1 (keyboard) unmasked\n");
     
     keyboard_init(); //  IDT(0x21=33)へIRQ1（キーボード）処理を登録
+    kputs("Keyboard interrupt handler registered\n");
+    
     exception_init(); // IDT(0〜31=0x00〜0x1F)へCPU例外処理を登録(panic)
+    kputs("CPU exception handlers registered\n");
     
     init_syscall(); // IDT 0x80へシステムコールを登録　Linuxの様にint0x80経由でシステムコールを呼び出す
+    kputs("System call handler (int 0x80) registered\n");
     
     asm volatile("sti");  // 割り込みを有効にする(PIC初期化しないと割り込みが常時発生)
     
